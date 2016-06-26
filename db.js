876e774ca21392e77b16f461bdea69cac22f8af2
Sciensio <chris.colleran@gmail.com>
2016-06-25 20:01:58 -0600

    1

--- db.js
+++ db.js
@@ -9,6 +9,7 @@ function createConnection() {
   pg.defaults.ssl = true;
 
   pg.connect(process.env.DATABASE_URL, function(err,client){
+    console.log("===create connection");
     if (err) {
         console.console.error(err);
         deferred.reject(err);
@@ -20,27 +21,31 @@ function createConnection() {
 
 function newUser(bot) {
   var deferred = Q.defer();
-
+  console.log("===creating connection");
   createConnection()
     .then (function (client) {
-          client.query('insert into Attendees (SmoochId, Unsubscribed, UnsubscribedDate, CreatedDate) values ($1,$2, null, CURRENT_TIMESTAMP);', [bot.userId, 'f'],
-          function(err,result) {
-              if (err) {
-                  if (err.code == '23505'){
-                      console.log("===user already exists: ", bot.userId);
-                      deferred.reject(err);
-                  }
-                  else{
-                      console.log("===Unknown error: ", err);
-                      deferred.reject(err);
-                  }
-              } else {
-                  (console.log('=== userId ', bot.userId));
-                  (console.log('=== record ', JSON.stringify(result.rows[0])));
-                  deferred.resolve(results);
+      client.query('insert into Attendees (SmoochId, Unsubscribed, UnsubscribedDate, CreatedDate) values ($1,$2, null, CURRENT_TIMESTAMP);', [bot.userId, 'f'],
+      function(err,result) {
+          if (err) {
+              if (err.code == '23505'){
+                  console.log("===user already exists: ", bot.userId);
+                  deferred.reject(err);
               }
-          });
+              else{
+                  console.log("===Unknown error: ", err);
+                  deferred.reject(err);
+              }
+          } else {
+              (console.log('=== userId ', bot.userId));
+              (console.log('=== record ', JSON.stringify(result.rows[0])));
+              deferred.resolve(results);
+          }
       });
+    })
+    .fail(function (err){
+      console.error(JSON.stringify(err));
+      deferred.reject(err);
+    });
   return deferred.promise;
 }
 
