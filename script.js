6ecf7197d5ea42d183f5231293318f69e7395912
Sciensio <chris.colleran@gmail.com>
2016-06-26 16:03:16 -0600

    fnapply

--- script.js
+++ script.js
@@ -73,7 +73,7 @@ module.exports = new Script({
             console.log("===bot user ",bot.userId);
 
             console.log("===before db");
-            promises.ninvoke(newUser(bot));
+            promises.fnapply(newUser(bot));
             console.log("===after db");
 
             let upperText = message.text.trim().toUpperCase();
