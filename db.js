0c10e7fcbf03feb7545baa554c5b57d0a3cc4e55
Sciensio <chris.colleran@gmail.com>
2016-06-25 18:00:07 -0600

    never work

--- db.js
+++ db.js
@@ -4,30 +4,39 @@ var request = require("request");
 
 const scriptRules = require('./script.json');
 
+function createConnection() {
+  var deferred = Q.defer();
+  pg.defaults.ssl = true;
+
+  pg.connect(process.env.DATABASE_URL, function(err,client){
+    if (err) {
+        console.console.error(err);
+        deferred.reject(err);
+    }
+    console.log("==db connection created");
+    deferred.resolve(pg);
+});
 
 function newUser(bot) {
   var deferred = Q.defer();
-  pg.defaults.ssl = true;
-  pg.connect(process.env.DATABASE_URL, function(err,client){
-      if (err) throw err;
-      console.log('====Connected to postgres!!!!!');
 
-      client
-          .query('insert into Attendees (SmoochId, Unsubscribed, UnsubscribedDate, CreatedDate) values ($1,$2, null, CURRENT_TIMESTAMP);', [bot.userId, 'f'],
+  getConnection()
+    .then (function (client) {
+          client.query('insert into Attendees (SmoochId, Unsubscribed, UnsubscribedDate, CreatedDate) values ($1,$2, null, CURRENT_TIMESTAMP);', [bot.userId, 'f'],
           function(err,result) {
               if (err) {
                   if (err.code == '23505'){
                       console.log("===user already exists: ", bot.userId);
+                      deferred.reject(err);
                   }
                   else{
                       console.log("===Unknown error: ", err);
+                      deferred.reject(err);
                   }
               } else {
                   (console.log('=== userId ', bot.userId));
-                  success: funtion() {
-                    (console.log('=== record ', JSON.stringify(result.rows[0])));
-                    deferred.resolve();
-                  }
+                  (console.log('=== record ', JSON.stringify(result.rows[0])));
+                  deferred.resolve(results);
               }
           });
   });
