695829a43b3d132d09a803263705bd7be06b697b
Sciensio <chris.colleran@gmail.com>
2016-06-26 14:01:21 -0600

    7

--- db.js
+++ db.js
@@ -45,7 +45,8 @@ function newUser(bot) {
               (console.log('=== record ', JSON.stringify(result.rows[0])));
           }
           deferred.resolve(results);
-      }));
+      });
+    );
     //})
     //.fail(function (err){
     //  console.log("===error");
