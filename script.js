69bc3e2067cbca981fadb4c831e3b83af4060d12
Sciensio <chris.colleran@gmail.com>
2016-06-26 18:46:20 -0600

    code for support = silence

--- script.js
+++ script.js
@@ -43,8 +43,8 @@ module.exports = new Script({
                 switch (upperText) {
                     case "CONNECT ME":
                         return bot.setProp("silent", true);
-                    case "@SUPPORT":
-                        return bot.setProp("silent", true);
+                    //case "@SUPPORT":
+                    //    return bot.setProp("silent", true);
                     case "DISCONNECT":
                         return bot.setProp("silent", false);
                     case "@ACE":
@@ -129,6 +129,8 @@ module.exports = new Script({
                             return bot.say(line);
                         });
                     });
+                    //if it was support then go silent
+                    if (response = "@support"){return bot.setProp("silent", true);}
                 });
 
                 return p.then(() => 'speak');
