e757f456c79823fbc46a3c3038c40950ec2b61e1
Sciensio <chris.colleran@gmail.com>
2016-06-27 09:52:38 -0600

    d

--- db.js
+++ db.js
@@ -35,7 +35,7 @@ function newUser(bot) {
             if (err) {
                 if (err.code == '23505'){
                     console.log("===user already exists: ", bot.userId);
-                    deferred.resolve(result);
+                    //deferred.resolve(result);
                 }
                 else{
                     console.error(err);
