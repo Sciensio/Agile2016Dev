78616cead169d251414b1d389b1a0f59ba6a9f03
Sciensio <chris.colleran@gmail.com>
2016-06-30 16:42:20 -0600

    adding debug code

--- script.js
+++ script.js
@@ -70,6 +70,7 @@ module.exports = new Script({
                 var fulfillmentSpeech;
                 var simplified;
                 promises.push(nlp(message.text, bot.userId));
+                console.log("===preprocess message",message.text);
 
                 Q.all(promises).then(function(responses) {
                     // response is the JSON from API.ai
