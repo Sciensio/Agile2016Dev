799503ae4500c1dd9dc2d8949dce9a74a2facfc0
Sciensio <chris.colleran@gmail.com>
2016-06-30 17:41:58 -0600

    change debug to payload

--- heroku/index.js
+++ heroku/index.js
@@ -110,7 +110,7 @@ console.log("=in app.post");
         msg = messages[0];
     } else {
         msg = req.body.postbacks[0];
-        console.log("=== POSTBACK",msg.action.text);
+        console.log("=== POSTBACK",msg.action.payload);
         msg.text = msg.action.text;
     }
 
