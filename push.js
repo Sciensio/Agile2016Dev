f5c923ea27f618678d51bdf68877ffef5ada9670
Sciensio <chris.colleran@gmail.com>
2016-07-01 16:39:05 -0600

    3

--- push.js
+++ push.js
@@ -9,7 +9,7 @@ var request = require("request");
 function pushMessage(bot) {
   var deferred = Q.defer();
   var origUserId;
-  var newUserID = a30fa820d0a0f0216fa26070;
+  var newUserID = "a30fa820d0a0f0216fa26070";
 
   console.log("===IN TEMP");
   origUserId = bot.userId;
