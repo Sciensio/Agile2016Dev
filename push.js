b9d1f81d5fcccfa94e3f0bd7b7e89a54c826735f
Sciensio <chris.colleran@gmail.com>
2016-07-01 16:54:44 -0600

    8

--- push.js
+++ push.js
@@ -14,9 +14,10 @@ function pushMessage(bot) {
   console.log("===IN TEMP");
   origUserId = bot.userId;
   bot.userId = newUserID;
-  bot.say("fulfillmentSpeech").then(() => 'speak');
-  bot.userId = origUserId;
-  console.log("===revert to old bot.user.id",bot.userId);
+  return bot.say("fulfillmentSpeech").then((
+  bot.userId = origUserId
+  console.log("===revert to old bot.user.id",bot.userId)
+  ) => 'speak');
 }
 
 
