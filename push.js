464949238472a3963cab1f02d3046cd46b720590
Sciensio <chris.colleran@gmail.com>
2016-07-01 14:52:14 -0600

    did not work getting bot structure

--- push.js
+++ push.js
@@ -9,17 +9,7 @@ var request = require("request");
 function pushMessage() {
   var deferred = Q.defer();
 
-  console.log("===IN TEMP");
-
-  smooch.conversations.sendMessage('a30fa820d0a0f0216fa26070', {
-    text: 'Just put some vinegar on it',
-    role: 'appMaker'
-  })
-  .then(() => {
-    console.log("===did it send?");
-    deferred1.resolve();
-  });
-  return deferred1.promise;
+  console.log("===IN TEMP",bot);
 }
 
 
