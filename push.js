50237815c65a27a4058945f0c46d9ef53cdf5bda
Sciensio <chris.colleran@gmail.com>
2016-07-04 12:35:34 -0600

    bubmling

--- push.js
+++ push.js
@@ -33,8 +33,8 @@ function pushConv(bot, response) {
     client
       .query('SELECT SmoochId FROM attendees;')
         .on('row', function(row){
-          console.log("===SmoochId ",row.value);
-          bot.userId = JSON.stringify(row.value);
+          console.log("===SmoochId ",row);
+          bot.userId = JSON.stringify(row);
           return bot.say("push message").then(() => 'speak');
       })
   });
