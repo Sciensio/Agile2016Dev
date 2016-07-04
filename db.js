cbc37b0250e1e0ef3a847ed3d7745f69ef2c3526
Sciensio <chris.colleran@gmail.com>
2016-07-04 09:00:42 -0600

    Change db connection code

--- db.js
+++ db.js
@@ -4,54 +4,31 @@ const _ = require('lodash');
 const Script = require('smooch-bot').Script;
 var request = require("request");
 
-//const scriptRules = require('./script.json');
-//var LOG_PREFIX = '[CONN] - ';
-
-//undone add db connection pooling
-
-function createConnection() {
-  var deferred = Q.defer();
-
-  pg.defaults.ssl = true;
-  pg.connect(process.env.DATABASE_URL, function(err,result){
-    if (err) {
-        console.error(err);
-        deferred.reject(err);
-    }
-    console.log("===db connection created");
-    deferred.resolve(pg);
-  });
-  return deferred.promise;
-}
-
 function newUser(bot) {
   var deferred = Q.defer();
 
   console.log("===creating connection");
-  createConnection()
-    .then (function(pg) {
-      client
-      .query('insert into Attendees (SmoochId, Unsubscribed, UnsubscribedDate, CreatedDate) values ($1,$2, null, CURRENT_TIMESTAMP);', [bot.userId, 'f'],
-        function(err,result) {
-            if (err) {
-                if (err.code == '23505'){
-                    console.log("===user already exists: ", bot.userId);
-                    //deferred.resolve(result);
-                }
-                else{
-                    console.error(err);
-                    deferred.reject(err);
-                }
-            } else {
-                (console.log('=== userId ', bot.userId));
-                deferred.resolve(result);
-            }
-        })
-    //})
-    //.fail(function(err){
-    //    console.error("===bogus error here ",err.code);
-        //deferred.reject(err);
-    });
+  pg.defaults.ssl = true;
+  pg.connect(process.env.DATABASE_URL, function(err, client, done){
+    client.query('insert into Attendees (SmoochId, Unsubscribed, UnsubscribedDate, CreatedDate) values ($1,$2, null, CURRENT_TIMESTAMP);',
+      [bot.userId, 'f'],
+      function(err,result) {
+        done();
+          if (err) {
+              if (err.code == '23505'){
+                  console.log("===user already exists: ", bot.userId);
+                  //deferred.resolve(result);
+              }
+              else{
+                  console.error(err);
+                  deferred.reject(err);
+              }
+          } else {
+              (console.log('=== userId ', bot.userId));
+              deferred.resolve(result);
+          }
+    })
+  });
   return deferred.promise;
 }
 
