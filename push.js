b5c380700b5fd5090eb1b3f9f1dcb87f33f27c37
Sciensio <chris.colleran@gmail.com>
2016-07-05 12:45:36 -0600

    done again

--- push.js
+++ push.js
@@ -42,7 +42,7 @@ function pushConv(bot, response) {
           newBot.userId = row.smoochid;
           return newBot.say(newBot.userId).then(console.log("===bot.userId ",newBot.userId),() => 'speak');
         })
-      .done(client);
+      done(client);
   });
   console.log("=== ",bot);
 }
