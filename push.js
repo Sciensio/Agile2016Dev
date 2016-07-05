cf34ba5bc4c431256a518113862381c32221adbd
Sciensio <chris.colleran@gmail.com>
2016-07-05 12:13:39 -0600

    commenting out old code

--- push.js
+++ push.js
@@ -8,20 +8,20 @@ var request = require("request");
 
 function pushMessage(bot) {
   var deferred = Q.defer();
-  var origUserId;
-  var newUserID = "a30fa820d0a0f0216fa26070";
+//  var origUserId;
+//  var newUserID = "a30fa820d0a0f0216fa26070";
 
-  console.log("===IN TEMP");
-  origUserId = bot.userId;
-  bot.userId = newUserID;
-  return bot.say("fulfillmentSpeech")
-    .then(
-      bot.userId = origUserId,
-      console.log("===revert to old bot.user.id",bot.userId),
-      () => 'speak'
-    );
+//  console.log("===IN TEMP");
+  //origUserId = bot.userId;
+  //bot.userId = newUserID;
+//  return bot.say("fulfillmentSpeech")
+//    .then(
+//      bot.userId = origUserId,
+//      console.log("===revert to old bot.user.id",bot.userId),
+//      () => 'speak'
+//    );
 
-}
+//}
 
 function pushConv(bot, response) {
   var deferred = Q.defer();
