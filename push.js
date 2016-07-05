c55dd5f5413e790a4aa7cbcc999492c408374870
Sciensio <chris.colleran@gmail.com>
2016-07-05 13:05:32 -0600

    tracing bot.userid

--- push.js
+++ push.js
@@ -26,24 +26,22 @@ var request = require("request");
 function pushConv(bot, response) {
   var deferred = Q.defer();
   var newBot = bot;
-  var botUser = bot.userId;
-  var queryText = "SELECT SmoochId FROM attendees WHERE unsubscribed = FALSE AND SmoochId != 'a30fa820d0a0f0216fa26070' ;";
+  //var botUser = bot.userId;
+  //var queryText = "SELECT SmoochId FROM attendees WHERE unsubscribed = FALSE AND SmoochId != 'a30fa820d0a0f0216fa26070' ;";
 
   console.log("===creating pushconv connection ",queryText);
-  //origUserId = newBot.userId;
   pg.defaults.ssl = true;
   pg.connect(process.env.DATABASE_URL, function(err, client, done){
     console.log("===bot",newBot);
     client
       .query('SELECT SmoochId FROM attendees WHERE unsubscribed = FALSE;')
-      //.query(queryText)
         .on('row', function(row){
           console.log("===SmoochId ",row.smoochid);
           newBot.userId = row.smoochid;
-          newBot.say(newBot.userId).then(console.log("===bot.userId ",newBot.userId),() => 'speak');
+          newBot.say(newBot.userId).then(console.log("===newBot.userId ",newBot.userId),() => 'speak');
         })
   });
-  console.log("=== ",bot);
+  console.log("=== did bot change",bot);
 }
 
 
