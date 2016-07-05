0fc26dae2eb92865a75e34db538811d3ad07b5ae
Sciensio <chris.colleran@gmail.com>
2016-07-05 13:32:21 -0600

    step 2

--- push.js
+++ push.js
@@ -7,28 +7,9 @@ var Q = require("q");
 var request = require("request");
 var extend = require('util')._extend;
 
-//function pushMessage(bot) {
-//  var deferred = Q.defer();
-//  var origUserId;
-//  var newUserID = "a30fa820d0a0f0216fa26070";
-
-//  console.log("===IN TEMP");
-  //origUserId = bot.userId;
-  //bot.userId = newUserID;
-//  return bot.say("fulfillmentSpeech")
-//    .then(
-//      bot.userId = origUserId,
-//      console.log("===revert to old bot.user.id",bot.userId),
-//      () => 'speak'
-//    );
-
-//}
-
 function pushConv(bot, response) {
   var deferred = Q.defer();
-  var newBot = bot;
-  //var botUser = bot.userId;
-  //var queryText = "SELECT SmoochId FROM attendees WHERE unsubscribed = FALSE AND SmoochId != 'a30fa820d0a0f0216fa26070' ;";
+  var newBot = extend({}, bot);
 
   console.log("===creating pushconv connection ");
   pg.defaults.ssl = true;
