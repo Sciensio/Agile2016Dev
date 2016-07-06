76a731c96dd880ee6f7a32da22604d084fd1928a
Sciensio <chris.colleran@gmail.com>
2016-07-06 12:15:18 -0600

    rollback

--- script.js
+++ script.js
@@ -42,7 +42,7 @@ module.exports = new Script({
 
             //Undone - currently only creates new user
             //needs to create conversation record and update it throughout the prcoess
-            console.log("===before db",bot.SmoochApiStore.Smooch.conversations);
+            console.log("===before db",bot);
             newUser(bot)
               .then (console.log("===after db", bot.userId))
 
