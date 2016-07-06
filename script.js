d4c60f7503dc9dfe4ccc9b2f0d9a8c6029f6face
Sciensio <chris.colleran@gmail.com>
2016-07-06 12:28:46 -0600

    bot.store.api

--- script.js
+++ script.js
@@ -42,7 +42,7 @@ module.exports = new Script({
 
             //Undone - currently only creates new user
             //needs to create conversation record and update it throughout the prcoess
-            console.log("===before db",bot.store.api.get(bot.userId));
+            console.log("===before db",bot.store.api);
             newUser(bot)
               .then (console.log("===after db", bot.userId))
 
