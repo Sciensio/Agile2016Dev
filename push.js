cbd7f51b160d5da8b6d4f638022509a752a4fcbe
Sciensio <chris.colleran@gmail.com>
2016-07-07 06:49:36 -0600

    Revert "take 2 of not passing in bot to push"
    This reverts commit 52373f596648a04d70d9325f018ddfa7f2af43bb.

--- push.js
+++ push.js
@@ -1,41 +1,16 @@
 'use strict';
 
 const _ = require('lodash');
+//const Script = require('smooch-bot').Script;
 var pg = require('pg');
+//var Q = require("q");
+//var request = require("request");
 var extend = require('util')._extend;
 
-var pg = require('pg');
-const smoochBot = require('smooch-bot');
-const MemoryLock = smoochBot.MemoryLock;
-const SmoochApiStore = smoochBot.SmoochApiStore;
-const SmoochApiBot = smoochBot.SmoochApiBot;
-const StateMachine = smoochBot.StateMachine;
-const app = require('../app');
-const script = require('../script');
-const SmoochCore = require('smooch-core');
-
-const jwt = require('../jwt');
-const fs = require('fs');
-var pushConv = require('../push');
-
-const name = 'A16';
-const avatarUrl = 'https://raw.githubusercontent.com/Sciensio/Agile2016Dev/master/img/agile-alliance-logo-bot.png';
-const store = new SmoochApiStore({
-    jwt
-});
-const lock = new MemoryLock();
-const userId = 'a30fa820d0a0f0216fa26070';
-
-var newBot = new SmoochApiBot({
-    name,
-    avatarUrl,
-    lock,
-    store,
-    userId
-});
-
-function pushConv(message, response) {
-//  var newBot = bot;
+function pushConv(bot, message, response) {
+  //var deferred = Q.defer();
+  //var newBot = extend({}, bot);
+  var newBot = bot;
 
   console.log("===creating pushconv connection ");
   pg.defaults.ssl = true;
@@ -49,8 +24,10 @@ function pushConv(message, response) {
           return newBot.say(message).then(console.log("===newBot.userId ",newBot.userId),() => 'speak');
         })
       done();
+      //pg.end();
   });
   console.log("=== did bot change",bot);
 }
+
 
 module.exports = pushConv;
