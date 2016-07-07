18d4976cd579e2a01962ad0ea0ff3c708fad46b1
Sciensio <chris.colleran@gmail.com>
2016-07-07 07:18:13 -0600

    go back

--- push.js
+++ push.js
@@ -4,41 +4,13 @@ const _ = require('lodash');
 var pg = require('pg');
 var extend = require('util')._extend;
 
-const smoochBot = require('smooch-bot');
-const MemoryLock = smoochBot.MemoryLock;
-const SmoochApiStore = smoochBot.SmoochApiStore;
-const SmoochApiBot = smoochBot.SmoochApiBot;
-const StateMachine = smoochBot.StateMachine;
-const app = require('./app');
-const script = require('./script');
-const SmoochCore = require('smooch-core');
-
-const jwt = require('./jwt');
-const fs = require('fs');
-
-const name = 'A16';
-const avatarUrl = 'https://raw.githubusercontent.com/Sciensio/Agile2016Dev/master/img/agile-alliance-logo-bot.png';
-const store = new SmoochApiStore({
-    jwt
-});
-const lock = new MemoryLock();
-const userId = 'a30fa820d0a0f0216fa26070';
-
-var newBot1 = new SmoochApiBot({
-    name,
-    avatarUrl,
-    lock,
-    store,
-    userId
-});
-
-function pushConv(message, response) {
-  //var newBot = bot;
+function pushConv(bot, message, response) {
+  var newBot = bot;
 
   console.log("===creating pushconv connection ");
   pg.defaults.ssl = true;
   pg.connect(process.env.DATABASE_URL, function(err, client, done){
-    console.log("===bot",newBot);
+    console.log("===bot");
     client
       .query('SELECT SmoochId FROM attendees WHERE unsubscribed = FALSE;')
         .on('row', function(row){
