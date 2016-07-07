9da1469f581f8558a303c52817b1e4bda360cbf2
Sciensio <chris.colleran@gmail.com>
2016-07-07 06:17:51 -0600

    changing push to not need to be passed the current bot
    this will make it much better elim potential bug that existed

--- push.js
+++ push.js
@@ -2,16 +2,44 @@
 
 const _ = require('lodash');
 const Script = require('smooch-bot').Script;
+
+const smoochBot = require('smooch-bot');
+const MemoryLock = smoochBot.MemoryLock;
+const SmoochApiStore = smoochBot.SmoochApiStore;
+const SmoochApiBot = smoochBot.SmoochApiBot;
+const StateMachine = smoochBot.StateMachine;
+const app = require('../app');
+//const script = require('../script');
+const SmoochCore = require('smooch-core');
+
+const jwt = require('../jwt');
+const fs = require('fs');
+
+const name = 'A16';
+const avatarUrl = 'https://raw.githubusercontent.com/Sciensio/Agile2016Dev/master/img/agile-alliance-logo-bot.png';
+const store = new SmoochApiStore({
+    jwt
+});
+const lock = new MemoryLock();
+const userId = 'a30fa820d0a0f0216fa26070';
+
+var newBot = new SmoochApiBot({
+    name,
+    avatarUrl,
+    lock,
+    store,
+    userId
+});
+
 var pg = require('pg');
 var Q = require("q");
 var request = require("request");
 var extend = require('util')._extend;
 
-function pushConv(bot, message, response) {
+function pushConv(message, response) {
   var deferred = Q.defer();
   //var newBot = extend({}, bot);
-  var newBot = bot;
-
+  //var newBot = bot;
   console.log("===creating pushconv connection ");
   pg.defaults.ssl = true;
   pg.connect(process.env.DATABASE_URL, function(err, client, done){
@@ -26,8 +54,7 @@ function pushConv(bot, message, response) {
       done();
       //pg.end();
   });
-  console.log("=== did bot change",bot);
+//  console.log("=== did bot change",bot);
 }
 
-
-module.exports = pushConv;//pushMessage;
+module.exports = pushConv;
