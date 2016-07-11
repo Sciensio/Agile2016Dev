fd4007b6bdaa7fd1340d8442c530a946350f85d9
Sciensio <chris.colleran@gmail.com>
2016-07-11 14:51:19 -0600

    2

--- script.js
+++ script.js
@@ -193,7 +193,7 @@ module.exports = new Script({
             {
                     //these are answers that we intercept because we do not like the domain answers
                     //and it does not appear that we can customize these items
-                    var test1 = _.filter(smalltalkSub, {phrase: [{simplified: simplified}] });
+                    var test1 = _.filter(summary.data, {phrase: [{simplified: simplified}] });
                     console.log('^^^^ phrase', test1);
 
                     if (fulfillmentSpeech && source === 'domains')
