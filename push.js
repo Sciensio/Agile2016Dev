981fac54e7ba68973152a2925a61eee13e78c2e5
Sciensio <chris.colleran@gmail.com>
2016-07-12 13:43:45 -0600

    this will break stuff, but kristi needs the updated json file

--- push.js
+++ push.js
@@ -4,10 +4,40 @@ const _ = require('lodash');
 var pg = require('pg');
 var extend = require('util')._extend;
 
-function pushConv(newBot, message, response) {
-  //var newBot = bot;
+//postgress connection
+var pg = require('pg');
+var Pool = require('pg').Pool;
+var pool = new Pool ({
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  host: process.env.DB_HOST,
+  database: process.env.DB_DATABASE,
+  max: 10,
+  idleTimeoutMillis: 1000
+});
 
-  console.log("===creating pushconv connection ");
+pool.on('error', function(e, client) {
+    console.log('|| Error in DB pool: ',e );
+})
+
+function schedConv(newBot, response) {
+  pool.connect(function(err, client, release) {
+  var query1 = client.query("SELECT message FROM batchmessage WHERE sendtime > '07/12/2016' ORDER BY sendtime");
+    query1.on('row', function(row1) {
+      var query2 = client.query("SELECT DISTINCT smoochid FROM conversation;");
+        query2.on('row',function(row2) {
+            release();
+            newBot.userId = row2.smoochid;
+            console.log('[] message: ',row1.message);
+            return newBot.say(row1.message).then(console.log("[] Attendee ",row2.smoochid," received message"),() => 'speak');
+        })
+    });
+  });
+}
+
+function adhocConv(newBot, message, response) {
+
+  console.log("|| creating pushconv connection ");
   pg.defaults.ssl = true;
   pg.connect(process.env.DATABASE_URL, function(err, client, done){
     console.log("===bot");
