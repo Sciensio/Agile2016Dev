b5b3670c820efce001698f0e602ac86b58c06d45
Sciensio <chris.colleran@gmail.com>
2016-07-11 19:20:22 -0600

    cutting wait time for messages

--- script.js
+++ script.js
@@ -272,7 +272,7 @@ module.exports = new Script({
                     line = line.trim();
                     p = p.then(function() {
                         console.log("=== p line",line);
-                        return wait(500).then(function() {
+                        return wait(100).then(function() {
                             return bot.say(line);
                         });
                     });
