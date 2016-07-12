eef1af10d9276ff9d41bcf4d7a5945358f00d0d4
Sciensio <chris.colleran@gmail.com>
2016-07-12 16:32:27 -0600

    3

--- script.js
+++ script.js
@@ -283,7 +283,10 @@ module.exports = new Script({
 
             return updateSilent()
               .then(console.log('--updateSilent step 1'))
-              .then(if (upperText !== '/SUPPORT') {getSilent})
+              //.then(getSilent)
+              .then(function (upperText){
+                if (upperText !== '/SUPPORT') {(getSilent)});
+              })
               .then(console.log('--updateSilent step 2'))
               .then(processMessage);
         }
