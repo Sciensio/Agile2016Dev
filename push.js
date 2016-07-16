ded0a971bf876f619eac3481742e01f3e4f4332d
Sciensio <chris.colleran@gmail.com>
2016-07-16 17:24:47 -0600

    taking out comments

--- push.js
+++ push.js
@@ -21,16 +21,16 @@ pool.on('error', function(e, client) {
 });
 
 function schedConv(newBot, response) {
-  console.log("< push.sched");
+  //console.log("< push.sched");
   pool.connect(function(err, client, release) {
-    console.log("pool connected");
+    //console.log("pool connected");
     var query1 = client.query("SELECT message FROM batchmessage WHERE sendtime >= CURRENT_TIMESTAMP - INTERVAL '299 seconds' AND sendtime <= CURRENT_TIMESTAMP + INTERVAL '5 minutes' ORDER BY sendtime");
       query1.on('row', function(row1) {
         var query2 = client.query("SELECT DISTINCT smoochid FROM conversation;");
           query2.on('row',function(row2) {
               release();
               newBot.userId = row2.smoochid;
-              console.log('|| message: ',row1.message);
+              //console.log('|| message: ',row1.message);
               //return newBot.say(row1.message).then(console.log("|| Attendee ",row2.smoochid," received message"),() => 'speak');
           })
       });
@@ -43,14 +43,14 @@ function adhocConv(newBot, message, response) {
     query.on('row', function(row){
       release();
       newBot.userId = row.smoochid;
-      console.log("|| Sending ad hoc message toSmoochId ",row.smoochid);
+      //console.log("|| Sending ad hoc message toSmoochId ",row.smoochid);
       //return newBot.say(message).then(console.log("|| Attendee ",newBot.userId," was sent message:", message),() => 'speak');
     });
   });
 }
 
 function logConversation(msgLog) {
-  console.log("|| in db, msgLog",msgLog);
+  //console.log("|| in db, msgLog",msgLog);
   pool.connect(function(err, client, release) {
     client.query('insert into conversation (smoochid, received, usermessage, role, message_id, sourcetype, receivedtime, responsemessage, responsetype, responsetime) values ($1,$2, $3, $4, $5,$6, $7, $8, $9, $10);',
       [msgLog.smoochId, msgLog.received, msgLog.usermessage, msgLog.role, msgLog.message_id, msgLog.sourcetype, msgLog.receivedtime, msgLog.responsemessage, msgLog.responsetype, msgLog.responsetime]);
