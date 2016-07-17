a6c8186ead93d8a89f8fd4b334a8fb28307ec415
Sciensio <chris.colleran@gmail.com>
2016-07-17 13:58:04 -0600

    debug code

--- push.js
+++ push.js
@@ -8,8 +8,6 @@ var extend = require('util')._extend;
 var pg = require('pg');
 var Pool = require('pg').Pool;
 
-if (typeof pool === 'undefined') {
-console.log("new pool");
 var pool = new Pool ({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
@@ -18,9 +16,6 @@ var pool = new Pool ({
   max: process.env.DB_CONNECTION_LIMIT,
   idleTimeoutMillis: 1000
 });
-}
-
-console.log(typeof pool);
 
 pool.on('error', function(e, client) {
     console.log('|| Error in DB pool: ',e );
@@ -45,11 +40,12 @@ function schedConv(newBot, response) {
 
 function adhocConv(newBot, message, response) {
   pool.connect(function(err, client, done) {
-    console.log("|| Sending ad hoc message toSmoochId ",message);
     var query = client.query('SELECT DISTINCT smoochid FROM conversation;');
       done();
       query.on('row', function(row){
         newBot.userId = row.smoochid;
+        console.log("|| Sending ad hoc message toSmoochId ", row.smoochid);
+        
         return newBot.say(message).then(console.log("|| Attendee ",newBot.userId," was sent message:", message),() => 'speak');
         if(err) {
           return console.error("|| ", err);
