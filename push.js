21b08b576b4e9f405f3c5ea21a5bb47173d42602
Sciensio <chris.colleran@gmail.com>
2016-07-17 10:50:15 -0600

    back to ad hoc

--- push.js
+++ push.js
@@ -38,13 +38,16 @@ function schedConv(newBot, response) {
 }
 
 function adhocConv(newBot, message, response) {
-  pool.connect(function(err, client, release) {
+  pool.connect(function(err, client, done) {
     console.log("|| Sending ad hoc message toSmoochId ",message);
     var query = client.query('SELECT DISTINCT smoochid FROM conversation;');
+      done();
       query.on('row', function(row){
-        release();
         newBot.userId = row.smoochid;
         //return newBot.say(message).then(console.log("|| Attendee ",newBot.userId," was sent message:", message),() => 'speak');
+        if(err) {
+          return console.error("|| ", err);
+        }
       });
   });
 }
