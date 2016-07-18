d506ec7887eebe32d65616bf39d6b8e3795ce7e0
Sciensio <chris.colleran@gmail.com>
2016-07-18 16:26:17 -0600

    2

--- push.js
+++ push.js
@@ -19,7 +19,7 @@ pg.defaults.ssl = true;
             query2.on('row',function(row2) {
                 newBot.userId = row2.smoochid;
                 //console.log('|| message: ',row1.message);
-                return newBot.say(process.env.SCHED_PREFIX + row1.message).then(),() => 'speak');
+                return newBot.say(process.env.SCHED_PREFIX + row1.message).then(() => 'speak');
                 client.end();
             });
         });
