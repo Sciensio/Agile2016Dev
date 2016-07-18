79df0a6034b2f11fa3d8155ccc999afbfac5d50a
Sciensio <chris.colleran@gmail.com>
2016-07-18 08:37:26 -0600

    9

--- push.js
+++ push.js
@@ -33,7 +33,7 @@ pool.on('error', function(e, client) {
 
   function schedConv(newBot, response) {
       var query1 = pool.query("SELECT message FROM batchmessage WHERE sendtime >= CURRENT_TIMESTAMP - INTERVAL '299 seconds' AND sendtime <= CURRENT_TIMESTAMP + INTERVAL '5 minutes' ORDER BY sendtime");
-      console.log("|| rowCount is", query1.rows.length);
+      //console.log("|| rowCount is", query1.rows.length);
       query1.on('row', function(row1) {
         var query2 = pool.query("SELECT DISTINCT smoochid FROM conversation;");
           query2.on('row',function(row2) {
