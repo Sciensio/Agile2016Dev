6897380516e6d90340b356414f96499b310514e0
Sciensio <chris.colleran@gmail.com>
2016-07-18 06:20:00 -0600

    trying load test

--- script.js
+++ script.js
@@ -257,10 +257,12 @@ module.exports = new Script({
                       msgLog.responsetime = new Date();
                       msgLog.responsetype = 'API.AI Domain';
                       console.log("starting for");
-                      //for (var i = 0; i < 200; i++) {
+
+
+                      for (var i = 0; i < 200; i++) {
                         //console.log(">>>>>>>>>>>>> in test");
                         push.logConversation(msgLog);
-                      //}
+                      }
 
                       return bot.say(fulfillmentSpeech).then(() => 'speak');
                   }
