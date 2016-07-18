7f480c9f105275b4d33ad1190cca2b47eeb84819
Sciensio <chris.colleran@gmail.com>
2016-07-18 08:55:50 -0600

    12

--- push.js
+++ push.js
@@ -36,7 +36,6 @@ pool.on('error', function(e, client) {
     pool.connect(function(err, client, release) {
       var query1 = client.query("SELECT message FROM batchmessage WHERE sendtime >= CURRENT_TIMESTAMP - INTERVAL '299 seconds' AND sendtime <= CURRENT_TIMESTAMP + INTERVAL '5 minutes' ORDER BY sendtime");
         query1.on('row', function(row1) {
-          console.log("row count",row.rowCount);
           var query2 = client.query("SELECT DISTINCT smoochid FROM conversation;");
             query2.on('row',function(row2) {
                 release();
@@ -44,6 +43,9 @@ pool.on('error', function(e, client) {
                 console.log('|| message: ',row1.message);
                 return newBot.say(row1.message).then(console.log("|| Attendee ",row2.smoochid," received message"),() => 'speak');
             })
+        });
+        query.on('end', function(result) {
+          console.log(result.rows.length + ' rows were received');
         });
       });
   }
