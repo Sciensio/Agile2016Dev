1447af0c9ea10d71a8a50f0e5b448cebbe0a7fa5
Sciensio <chris.colleran@gmail.com>
2016-07-17 19:24:53 -0600

    moving release

--- push.js
+++ push.js
@@ -37,8 +37,8 @@ pool.on('error', function(e, client) {
       var query1 = client.query("SELECT message FROM batchmessage WHERE sendtime >= CURRENT_TIMESTAMP - INTERVAL '299 seconds' AND sendtime <= CURRENT_TIMESTAMP + INTERVAL '5 minutes' ORDER BY sendtime");
         query1.on('row', function(row1) {
           var query2 = client.query("SELECT DISTINCT smoochid FROM conversation;");
+          release();
             query2.on('row',function(row2) {
-                release();
                 newBot.userId = row2.smoochid;
                 console.log('|| message: ',row1.message);
                 return newBot.say(row1.message).then(console.log("|| Attendee ",row2.smoochid," received message"),() => 'speak');
