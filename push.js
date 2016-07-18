7c71c04a99ec5cf18d61d52e6c96440d38196c84
Sciensio <chris.colleran@gmail.com>
2016-07-18 08:28:45 -0600

    6

--- push.js
+++ push.js
@@ -32,27 +32,19 @@ pool.on('error', function(e, client) {
 });
 
   function schedConv(newBot, response) {
-    //console.log("< push.sched");
-    //pool.connect(function(err, client, release) {
-      //console.log("pool connected");
       var query1 = pool.query("SELECT message FROM batchmessage WHERE sendtime >= CURRENT_TIMESTAMP - INTERVAL '299 seconds' AND sendtime <= CURRENT_TIMESTAMP + INTERVAL '5 minutes' ORDER BY sendtime",
         function(err){
           if(err) console.log("!!!!!!!!!!!!!!! error in log ",err);
-        }
-        console.log("|| rowCount is", query1.rowCount);
-        if (query1.rowCount > 0) {
-          query1.on('row', function(row1) {
-            var query2 = pool.query("SELECT DISTINCT smoochid FROM conversation;");
-            //release();
-            //TODO: need to test for no rows and exit
-              query2.on('row',function(row2) {
-                  newBot.userId = row2.smoochid;
-                  console.log('|| message: ',row1.message);
-                  return newBot.say(row1.message).then(console.log("|| Attendee ",row2.smoochid," received message"),() => 'speak');
-              })
-          });
-      }
-      return);
+      });
+      console.log("|| rowCount is", query1.rowCount);
+      query1.on('row', function(row1) {
+        var query2 = pool.query("SELECT DISTINCT smoochid FROM conversation;");
+          query2.on('row',function(row2) {
+              newBot.userId = row2.smoochid;
+              console.log('|| message: ',row1.message);
+              return newBot.say(row1.message).then(console.log("|| Attendee ",row2.smoochid," received message"),() => 'speak');
+          })
+      });
   }
 
   function adhocConv(newBot, message, response) {
