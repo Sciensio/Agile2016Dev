eac6f4d385de03cfeab04cf94da82beb0c48842d
Sciensio <chris.colleran@gmail.com>
2016-07-18 08:07:55 -0600

    bug: schedConv not ending if not records

--- push.js
+++ push.js
@@ -33,20 +33,25 @@ pool.on('error', function(e, client) {
 
   function schedConv(newBot, response) {
     //console.log("< push.sched");
-    pool.connect(function(err, client, release) {
+    //pool.connect(function(err, client, release) {
       //console.log("pool connected");
-      var query1 = client.query("SELECT message FROM batchmessage WHERE sendtime >= CURRENT_TIMESTAMP - INTERVAL '299 seconds' AND sendtime <= CURRENT_TIMESTAMP + INTERVAL '5 minutes' ORDER BY sendtime");
-        query1.on('row', function(row1) {
-          var query2 = client.query("SELECT DISTINCT smoochid FROM conversation;");
-          release();
-          //TODO: need to test for no rows and exit
-            query2.on('row',function(row2) {
-                newBot.userId = row2.smoochid;
-                console.log('|| message: ',row1.message);
-                return newBot.say(row1.message).then(console.log("|| Attendee ",row2.smoochid," received message"),() => 'speak');
-            })
+      var query1 = pool.query("SELECT message FROM batchmessage WHERE sendtime >= CURRENT_TIMESTAMP - INTERVAL '299 seconds' AND sendtime <= CURRENT_TIMESTAMP + INTERVAL '5 minutes' ORDER BY sendtime"),
+        query1.on('error', function(error){
+          console.log("!!!!!!!!!!!!!!! error in log ",err);
         });
-      });
+        if (query1.rowCount > 0) {
+          query1.on('row', function(row1) {
+            var query2 = pool.query("SELECT DISTINCT smoochid FROM conversation;");
+            //release();
+            //TODO: need to test for no rows and exit
+              query2.on('row',function(row2) {
+                  newBot.userId = row2.smoochid;
+                  console.log('|| message: ',row1.message);
+                  return newBot.say(row1.message).then(console.log("|| Attendee ",row2.smoochid," received message"),() => 'speak');
+              })
+          });
+        return;
+      }
   }
 
   function adhocConv(newBot, message, response) {
