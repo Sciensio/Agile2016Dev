373b6f5e6c8e9ea4df20ce11e4a69cc08fd6f147
Sciensio <chris.colleran@gmail.com>
2016-07-20 16:54:42 -0600

    did not get moving on

--- script.js
+++ script.js
@@ -166,16 +166,16 @@ module.exports = new Script({
                   } else {
                     msgLog.responsetype = 'No match';
                     msgLog.responsetime = new Date();
-                    var noMsg = ''
-                    if (isMediaMessage === true) {
-                      noMsg = `I'm sorry I don't know how to respond to media yet.  😳   Type MENU or KEY for a list of things I can help you with.`;
-                    } else {
-                      noMsg = `I'm sorry that is not something I know.  😳   Type MENU or KEY for a list of things I can help you with.`;
-                    }
-                    msgLog.responsemessage = msg;
-                    console.log("msg not found: ", noMsg);
-                    return bot.say(msg).then(() => 'speak');
-                    //return bot.say(`I'm sorry that is not something I know.  😳   Type MENU or KEY for a list of things I can help you with.`).then(() => 'speak');
+                    //var noMsg = ''
+                    //if (isMediaMessage === true) {
+                    //  noMsg = `I'm sorry I don't know how to respond to media yet.  😳   Type MENU or KEY for a list of things I can help you with.`;
+                    //} else {
+                    //  noMsg = `I'm sorry that is not something I know.  😳   Type MENU or KEY for a list of things I can help you with.`;
+                    //}
+                    //msgLog.responsemessage = msg;
+                    //console.log("msg not found: ", noMsg);
+                    //return bot.say(msg).then(() => 'speak');
+                    return bot.say(`I'm sorry that is not something I know.  😳   Type MENU or KEY for a list of things I can help you with.`).then(() => 'speak');
                   }
                 });
               }, function(error) {
