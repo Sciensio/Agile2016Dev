c60f3ee7d2fdffba7f8618d42218f9a031831366
Sciensio <chris.colleran@gmail.com>
2016-07-22 17:12:07 -0600

    adding wait timer and adding err handling

--- push.js
+++ push.js
@@ -8,20 +8,34 @@
 var pg = require('pg');
 var Client = require('pg').Client;
 
+function wait(ms) {
+    return new Promise((resolve) => {
+        setTimeout(resolve, ms);
+    });
+}
+
 pg.defaults.ssl = true;
 
   function schedConv(newBot, response) {
     var client = new Client(process.env.DATABASE_URL);
     client.connect();
     var query1 = client.query("SELECT message FROM batchmessage WHERE sendtime >= CURRENT_TIMESTAMP - INTERVAL '299.999 seconds' AND sendtime <= CURRENT_TIMESTAMP + INTERVAL '5 minutes' ORDER BY sendtime");
-        query1.on('row', function(row1) {
+        query1.on('row', function(row1, err1) {
           var query2 = client.query("SELECT DISTINCT smoochid FROM conversation;");
-            query2.on('row',function(row2) {
+            query2.on('row',function(row2, err2) {
                 newBot.userId = row2.smoochid;
                 //console.log('|| message: ',row1.message);
-                return newBot.say(process.env.SCHED_PREFIX + row1.message).then(() => 'speak');
+                return wait(50).then(function() {
+                  return newBot.say(process.env.SCHED_PREFIX + row1.message).then(() => 'speak');
+                });
                 client.end();
+                if(err2) {
+                  return console.error("|| ", err2);
+                }
             });
+            if(err1) {
+              return console.error("|| ", err1);
+            }
         });
     client.on('drain', client.end.bind(client));
   }
