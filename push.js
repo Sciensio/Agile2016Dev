fcb0a88c75705bacdd8ab50f276cc4bbd284378a
Sciensio <chris.colleran@gmail.com>
2016-07-22 17:53:41 -0600

    7

--- push.js
+++ push.js
@@ -22,23 +22,14 @@ pg.defaults.ssl = true;
     var query1 = client.query("SELECT message FROM batchmessage WHERE sendtime >= CURRENT_TIMESTAMP - INTERVAL '299.999 seconds' AND sendtime <= CURRENT_TIMESTAMP + INTERVAL '5 minutes' ORDER BY sendtime");
         query1.on('row', function(row1, err1) {
           var query2 = client.query("SELECT DISTINCT smoochid FROM conversation;");
-            console.log(">>>>>>> scheduled message", row1);
             query2.on('row',function(row2, err2) {
-              console.log(">>>>>>> smoochid", row2);
                 newBot.userId = row2.smoochid;
                 //console.log('|| message: ',row1.message);
                 return wait(50).then(function() {
-                  console.log("<<<<<<< HERE in inner loop");
                   return newBot.say(process.env.SCHED_PREFIX + row1.message).then(console.log("|| Attendee ",newBot.userId," was sent message:", row1.message),() => 'speak');
                 });
                 client.end();
-                if(err2) {
-                  return console.error("|| error is inner loop: ", err2);
-                }
             });
-            //if(err1) {
-            //  return console.error("|| error in outer loop: ", err1);
-            //}
         });
     client.on('drain', client.end.bind(client));
   }
