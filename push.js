2e95a4b375ebc5a9d3b9b1286edf73af365d64b9
Sciensio <chris.colleran@gmail.com>
2016-07-22 16:40:31 -0600

    delete old resource file; add milliseconds to push file range

--- push.js
+++ push.js
@@ -13,7 +13,7 @@ pg.defaults.ssl = true;
   function schedConv(newBot, response) {
     var client = new Client(process.env.DATABASE_URL);
     client.connect();
-    var query1 = client.query("SELECT message FROM batchmessage WHERE sendtime >= CURRENT_TIMESTAMP - INTERVAL '299 seconds' AND sendtime <= CURRENT_TIMESTAMP + INTERVAL '5 minutes' ORDER BY sendtime");
+    var query1 = client.query("SELECT message FROM batchmessage WHERE sendtime >= CURRENT_TIMESTAMP - INTERVAL '299.999 seconds' AND sendtime <= CURRENT_TIMESTAMP + INTERVAL '5 minutes' ORDER BY sendtime");
         query1.on('row', function(row1) {
           var query2 = client.query("SELECT DISTINCT smoochid FROM conversation;");
             query2.on('row',function(row2) {
