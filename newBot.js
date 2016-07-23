fbd01837aa14d002a4fe9730f9e86d2d258d6d52
Sciensio <chris.colleran@gmail.com>
2016-07-23 07:09:55 -0600

    12

--- newBot.js
+++ newBot.js
@@ -37,7 +37,7 @@ function newBot_msg(type, message) {
 
 
     //SmoochCore.smooch.appUsers.get(userId).then((response) => {
-      console.log(smooch);
+      console.log(appUsers.get(userId));
     //});
 
 
