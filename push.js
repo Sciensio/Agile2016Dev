544e29682f709b06b7bd56de6114a1fa27858947
Sciensio <chris.colleran@gmail.com>
2016-07-23 09:32:34 -0600

    back to release state

--- push.js
+++ push.js
@@ -23,15 +23,10 @@ function wait(ms) {
     var query1 = client.query("SELECT message FROM batchmessage WHERE sendtime >= CURRENT_TIMESTAMP - INTERVAL '299.999 seconds' AND sendtime <= CURRENT_TIMESTAMP + INTERVAL '5 minutes' ORDER BY sendtime");
         query1.on('row', function(row1) {
           //var query2 = client.query("SELECT DISTINCT smoochid FROM conversation;");
-          var query2 = client.query("select smoochid from conversation WHERE smoochid = 'a30fa820d0a0f0216fa26070' LIMIT 20;");
-          var i = 1;
+          var query2 = client.query("select distinct smoochid from conversation;");
             query2.on('row',function(row2) {
                 newBot.userId = row2.smoochid;
-                return wait(500).then(function() {
-                  return newBot.say(process.env.SCHED_PREFIX + row1.message + " " + i).then(console.log("|| ",i),() => 'speak')
-                    //.then(console.log(i))
-                    .then(i = i+1);
-                  //.then(client.end());
+                  return newBot.say(process.env.SCHED_PREFIX + row1.message).then(console.log("|| ",i),() => 'speak')
                   if(err) {
                     return console.error("|| ", err);
                   }
@@ -39,9 +34,7 @@ function wait(ms) {
                 });
             });
         });
-    //return wait(10000).then(function() {
       client.on('drain', client.end.bind(client));
-    //});
   }
 
 module.exports = {schedConv};
