285d67a525c2f0ced6719b04ce22e9d9c84115e8
Sciensio <chris.colleran@gmail.com>
2016-07-22 22:32:38 -0600

    6

--- push.js
+++ push.js
@@ -19,25 +19,19 @@ function wait(ms) {
 
   function schedConv(newBot, response) {
     var client = new Client(process.env.DATABASE_URL);
-    var mess = [];
     client.connect();
+    var mess = [];
     var query1 = client.query("SELECT message FROM batchmessage WHERE sendtime >= CURRENT_TIMESTAMP - INTERVAL '299.999 seconds' AND sendtime <= CURRENT_TIMESTAMP + INTERVAL '5 minutes' ORDER BY sendtime");
         query1.on('row', function(row1) {
           //var query2 = client.query("SELECT DISTINCT smoochid FROM conversation;");
           var query2 = client.query("select smoochid from conversation WHERE smoochid = 'a30fa820d0a0f0216fa26070' LIMIT 30;");
-          var i = 1;
             query2.on('row',function(row2) {
-                //row2.smoochid;
                 console.log("before adding to array");
                 mess.push(row2.smoochid, process.env.SCHED_PREFIX + row1.message);
                 console.log("after adding to array");
-                  //return newBot.say(process.env.SCHED_PREFIX + row1.message + " " + i).then(console.log("|| ",i),() => 'speak')
-                    //.then(console.log(i))
-                    //.then(i = i+1);
-                  //.then(client.end());
             });
+            console.log("before sendSched");
         });
-      console.log("before sendSched");
       sendSched(mess);
       client.on('drain', client.end.bind(client));
 
