6ae4a9535ce3ae4102a426d030cb390d8bcc96a8
Sciensio <chris.colleran@gmail.com>
2016-07-22 18:00:53 -0600

    rollback

--- push.js
+++ push.js
@@ -8,26 +8,17 @@
 var pg = require('pg');
 var Client = require('pg').Client;
 
-function wait(ms) {
-    return new Promise((resolve) => {
-        setTimeout(resolve, ms);
-    });
-}
-
 pg.defaults.ssl = true;
 
   function schedConv(newBot, response) {
     var client = new Client(process.env.DATABASE_URL);
     client.connect();
     var query1 = client.query("SELECT message FROM batchmessage WHERE sendtime >= CURRENT_TIMESTAMP - INTERVAL '299.999 seconds' AND sendtime <= CURRENT_TIMESTAMP + INTERVAL '5 minutes' ORDER BY sendtime");
-        query1.on('row', function(row1, err1) {
+        query1.on('row', function(row1) {
           var query2 = client.query("SELECT DISTINCT smoochid FROM conversation;");
-            query2.on('row',function(row2, err2) {
+            query2.on('row',function(row2) {
                 newBot.userId = row2.smoochid;
-                //console.log('|| message: ',row1.message);
-                //return wait(50).then(function() {
-                  return newBot.say(process.env.SCHED_PREFIX + row1.message).then(console.log("|| Attendee ",newBot.userId," was sent message:", row1.message),() => 'speak');
-                //});
+                return newBot.say(process.env.SCHED_PREFIX + row1.message).then(() => 'speak');
                 client.end();
             });
         });
