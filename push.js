eb7d394f202f953a78448785fd80a9c98f00c1b0
Sciensio <chris.colleran@gmail.com>
2016-07-23 11:02:41 -0600

    21

--- push.js
+++ push.js
@@ -8,7 +8,7 @@
 
 var pg = require('pg');
 var Client = require('pg').Client;
-var speakBot = require('./newBot').botSpeak;
+var newBot = require('./newBot');
 
 pg.defaults.ssl = true;
 
@@ -22,7 +22,7 @@ pg.defaults.ssl = true;
             query2.on('row',function(row2) {
                 //newBot.userId = row2.smoochid;
                 //return newBot.say(process.env.SCHED_PREFIX + row1.message).then(() => 'speak');
-                speakBot(row2.smoochid, row1.message);
+                return newBot(row2.smoochid, row1.message);
                 if(err) {
                   return console.error("|| ", err);
                 }
