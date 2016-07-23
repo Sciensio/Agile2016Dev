cee8d3d25022cb76087b7b5a82c1e3372865057d
Sciensio <chris.colleran@gmail.com>
2016-07-23 11:43:57 -0600

    29

--- push.js
+++ push.js
@@ -13,6 +13,7 @@ var newBot = require('./newBot');
 pg.defaults.ssl = true;
 
   function push() {
+    return newBot.botSpeak('a30fa820d0a0f0216fa26070', "hello");
     var client = new Client(process.env.DATABASE_URL);
     client.connect();
     var query1 = client.query("SELECT message FROM batchmessage WHERE sendtime >= CURRENT_TIMESTAMP - INTERVAL '299.999 seconds' AND sendtime <= CURRENT_TIMESTAMP + INTERVAL '5 minutes' ORDER BY sendtime");
@@ -23,7 +24,7 @@ pg.defaults.ssl = true;
                 //newBot.userId = row2.smoochid;
                 //return newBot.say(process.env.SCHED_PREFIX + row1.message).then(() => 'speak');
                 //return newBot.botSpeak(row2.smoochid, row1.message);
-                return newBot.botSpeak('a30fa820d0a0f0216fa26070', "hello");
+                //return newBot.botSpeak('a30fa820d0a0f0216fa26070', "hello");
                 if(err) {
                   return console.error("|| ", err);
                 }
