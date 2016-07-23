4031d463949b6da94078c2214c1b95aebfad4328
Sciensio <chris.colleran@gmail.com>
2016-07-22 19:49:54 -0600

    add counter 8

--- push.js
+++ push.js
@@ -20,7 +20,7 @@ pg.defaults.ssl = true;
           var i = 1;
             query2.on('row',function(row2) {
                 newBot.userId = row2.smoochid;
-                newBot.say(process.env.SCHED_PREFIX + row1.message).then(() => 'speak')
+                newBot.say(process.env.SCHED_PREFIX + row1.message + " " + i).then(() => 'speak')
                   .then(console.log(i))
                   .then(i = i+1)
                   .then(return);
