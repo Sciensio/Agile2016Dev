87035f7e5a24e5c9dd7ea62042de119d3444d51b
Sciensio <chris.colleran@gmail.com>
2016-07-22 23:28:44 -0600

    restart 1

--- push.js
+++ push.js
@@ -20,36 +20,38 @@ function wait(ms) {
   function schedConv(newBot, response) {
     var client = new Client(process.env.DATABASE_URL);
     client.connect();
-    var mess = [{uid:"",msg:""}];
+    var mess = "";
+    var uid = [];
     var query1 = client.query("SELECT message FROM batchmessage WHERE sendtime >= CURRENT_TIMESTAMP - INTERVAL '299.999 seconds' AND sendtime <= CURRENT_TIMESTAMP + INTERVAL '5 minutes' ORDER BY sendtime");
-        //query1.on('row', function(row1) {
-          //var query2 = client.query("SELECT DISTINCT smoochid FROM conversation;");
-        //  var query2 = client.query("select smoochid from conversation WHERE smoochid = 'a30fa820d0a0f0216fa26070' LIMIT 30;");
-        //    query2.on('row',function(row2) {
+        query1.on('row', function(row1) {
+          mess = process.env.SCHED_PREFIX + row1.message
+        }
+        //var query2 = client.query("SELECT DISTINCT smoochid FROM conversation;");
+    var query2 = client.query("select smoochid from conversation WHERE smoochid = 'a30fa820d0a0f0216fa26070' LIMIT 30;");
+      query2.on('row',function(row2) {
                 //console.log("before adding to array");
-        //        mess.push(row2.smoochid, process.env.SCHED_PREFIX + row1.message);
+        mess.push(row2.smoochid);
                 //console.log("after adding to array");
-        //    });
+      });
         //    query2.on('end', function(result) {
                 //fired once and only once, after the last row has been returned and after all 'row' events are emitted
                 //in this example, the 'rows' array now contains an ordered set of all the rows which we received from postgres
         //        console.log(result.rowCount + ' rows were received');
-        //        sendSched(newBot, mess);
+      sendSched(newBot, uid, mess);
         //      })
         //});
-      console.log(query1.text);
+      console.log(mess);
       client.on('drain', client.end.bind(client));
 
   }
 
-
-  function sendSched (bot,msg) {
+  function sendSched (bot, uid, msg) {
     console.log("sendSched was called");
-    var arrayLength = msg.length;
+    var arrayLength = uid.length;
     for (var i = 0; i < arrayLength; i++) {
-        console.log(msg[i].uid);
-        bot.userId = msg[i].msg;
-        bot.say(msg[i][1]).then(console.log(msg[i][1]),() => 'speak');
+        console.log(msg);
+        bot.userId = uid[i];
+        bot.say(msg).then(console.log(msg),() => 'speak');
     }
   }
 
