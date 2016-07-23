516338767e4948f2956dedbd605a2d3ffa066629
Sciensio <chris.colleran@gmail.com>
2016-07-23 07:03:14 -0600

    8

--- newBot.js
+++ newBot.js
@@ -37,11 +37,11 @@ function newBot_msg(type, message) {
 
 
     //SmoochCore.smooch.appUsers.get(userId).then((response) => {
-      console.log(SmoochCore);
+      console.log(SmoochCore.appUsers);
     //});
 
 
-    
+
     return sched.schedConv(newBot);
   } else if (type === 'adhoc') {
     //console.log("> newbot, ad hoc");
