823e3ad7e495c5280c61a541557ce03b15b5abc6
Sciensio <chris.colleran@gmail.com>
2016-07-22 18:23:47 -0600

    me 30

--- push.js
+++ push.js
@@ -16,7 +16,7 @@ pg.defaults.ssl = true;
     var query1 = client.query("SELECT message FROM batchmessage WHERE sendtime >= CURRENT_TIMESTAMP - INTERVAL '299.999 seconds' AND sendtime <= CURRENT_TIMESTAMP + INTERVAL '5 minutes' ORDER BY sendtime");
         query1.on('row', function(row1) {
           //var query2 = client.query("SELECT DISTINCT smoochid FROM conversation;");
-          var query2 = client.query("select smoochid from conversation LIMIT 20;");
+          var query2 = client.query("select smoochid from conversation WHERE smoochid = 'a30fa820d0a0f0216fa26070' LIMIT 30;");
             query2.on('row',function(row2) {
                 newBot.userId = row2.smoochid;
                 return newBot.say(process.env.SCHED_PREFIX + row1.message).then(() => 'speak')
