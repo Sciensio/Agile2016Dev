8081acdc4f40d531db62e8762248cba6d420b2ca
Sciensio <chris.colleran@gmail.com>
2016-07-23 08:29:29 -0600

    ad hoc back to different users

--- log.js
+++ log.js
@@ -37,7 +37,7 @@ pool.on('error', function(e, client) {
           console.error("pool error: ",err);
         }
         //var query = client.query('SELECT DISTINCT smoochid FROM conversation;');
-        var query = client.query("select smoochid from conversation WHERE smoochid = 'a30fa820d0a0f0216fa26070' LIMIT 100;");
+        var query = client.query("select distinct smoochid from conversation order by smoochid ASC;");
           query.on('row', function(row, err){
             newBot.userId = row.smoochid;
             //console.log("|| Sending ad hoc message toSmoochId ", row.smoochid);
