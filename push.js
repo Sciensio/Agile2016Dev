da14f7164ec5c209f56dd304447fcc151da4930b
Sciensio <chris.colleran@gmail.com>
2016-07-22 20:42:34 -0600

    back to 20

--- push.js
+++ push.js
@@ -23,14 +23,14 @@ function wait(ms) {
     var query1 = client.query("SELECT message FROM batchmessage WHERE sendtime >= CURRENT_TIMESTAMP - INTERVAL '299.999 seconds' AND sendtime <= CURRENT_TIMESTAMP + INTERVAL '5 minutes' ORDER BY sendtime");
         query1.on('row', function(row1) {
           //var query2 = client.query("SELECT DISTINCT smoochid FROM conversation;");
-          var query2 = client.query("select smoochid from conversation WHERE smoochid = 'a30fa820d0a0f0216fa26070' LIMIT 30;");
+          var query2 = client.query("select smoochid from conversation WHERE smoochid = 'a30fa820d0a0f0216fa26070' LIMIT 20;");
           var i = 1;
             query2.on('row',function(row2) {
                 newBot.userId = row2.smoochid;
                 return wait(1000).then(function() {
-                  return newBot.say(process.env.SCHED_PREFIX + row1.message + " " + i).then(console.log("|| ",i),() => 'speak')
-                    .then(console.log(i))
-                    .then(i = i+1);
+                  return newBot.say(process.env.SCHED_PREFIX + row1.message + " " + i).then(console.log("|| ",i),() => 'speak');
+                    //.then(console.log(i))
+                    //.then(i = i+1);
                   //.then(client.end());
                 });
             });
