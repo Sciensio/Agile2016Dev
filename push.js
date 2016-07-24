b17b7ad8469e60db3282ca8b81be0995886a43ec
Sciensio <chris.colleran@gmail.com>
2016-07-24 14:44:04 -0400

    20

--- push.js
+++ push.js
@@ -27,8 +27,9 @@ function wait(ms) {
             query2.on('row',function(row2) {
                 newBot.userId = row2.smoochid;
                 wait(1000).then(
-                  return newBot.say(process.env.SCHED_PREFIX + row1.message).then(() => 'speak');
+                  newBot.say(process.env.SCHED_PREFIX + row1.message).then(() => 'speak');
                 )
+                return;
                   //if(err) {
                   //  return console.error("|| ", err);
                   //}
