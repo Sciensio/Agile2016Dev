448b7da94e95b96060f5e8b93722bba59c2c0f70
Sciensio <chris.colleran@gmail.com>
2016-07-25 13:38:43 -0400

    7

--- push.js
+++ push.js
@@ -35,6 +35,7 @@ function wait(ms) {
             var arrayLength = rows2.length;
             for (var i = 0; i < arrayLength; i++) {
             newBot.userId = rows2[i];
+            console.log("userId: ", newBot.userId);
             newBot.say(process.env.SCHED_PREFIX + row1.message)
               .then(() => {
                 'speak';
