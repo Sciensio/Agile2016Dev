0cfc46a472bb952f01d314d681a94cb3cbff1202
Sciensio <chris.colleran@gmail.com>
2016-07-25 13:49:59 -0400

    rollback

--- push.js
+++ push.js
@@ -1,8 +1,4 @@
-'use strict';
-
-//const _ = require('lodash');
-//var pg = require('pg');
-//var extend = require('util')._extend;
+uire('util')._extend;
 
 //postgress client connection
 
@@ -22,23 +18,14 @@ function wait(ms) {
     client.connect();
     var query1 = client.query("SELECT message FROM batchmessage WHERE sendtime >= CURRENT_TIMESTAMP - INTERVAL '299.999 seconds' AND sendtime <= CURRENT_TIMESTAMP + INTERVAL '5 minutes' ORDER BY sendtime");
         query1.on('row', function(row1) {
-          var query2 = client.query("SELECT smoochid FROM conversation WHERE smoochid = 'a30fa820d0a0f0216fa26070' LIMIT 30;");
-          //var query2 = client.query("select distinct smoochid from conversation;");
-          var rows2 = [];
+          //var query2 = client.query("SELECT smoochid FROM conversation WHERE smoochid = 'a30fa820d0a0f0216fa26070' LIMIT 30;");
+          var query2 = client.query("select distinct smoochid from conversation;");
             query2.on('row',function(row2) {
-              rows2.push(row2);
+                newBot.userId = row2.smoochid;
+                  console.log('newBot.userId: ', newBot.userId);
+                  newBot.say(process.env.SCHED_PREFIX + row1.message).then(() => 'speak');
             });
-          query2.on('end', function(result) {
-            console.log("in end");
-            var i = 0
-            var arrayLength = rows2.length;
-            for (var i = 0; i < arrayLength; i++) {
-              newBot.userId = rows2[i];
-              return newBot.say(process.env.SCHED_PREFIX + row1.message).then(() => 'speak');
-              console.log("after speak");
-            }
-          });
-      });
+        });
       client.on('drain', client.end.bind(client));
   }
 
