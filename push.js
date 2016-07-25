66dc9f5a7e8f008be53d02886b9e0219e7d2f812
Sciensio <chris.colleran@gmail.com>
2016-07-25 11:35:33 -0400

    18

--- push.js
+++ push.js
@@ -25,7 +25,7 @@ function wait(ms) {
           var query2 = client.query("SELECT smoochid FROM conversation WHERE smoochid = 'a30fa820d0a0f0216fa26070' LIMIT 30;");
           //var query2 = client.query("select distinct smoochid from conversation;");
             query2.on('row',function(row2) {
-              return wait(50).then(function() {
+              return wait(1000).then(function() {
                   newBot.userId = row2.smoochid;
                   newBot.say(process.env.SCHED_PREFIX + row1.message)
                     .then(() => {
