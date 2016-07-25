018e51e8c33eebe593011ec5157c896a7f68a47b
Sciensio <chris.colleran@gmail.com>
2016-07-25 18:06:35 -0400

    new direction 1

--- push.js
+++ push.js
@@ -5,7 +5,7 @@
 //var extend = require('util')._extend;
 
 //postgress client connection
-const _ = require('lodash');
+
 var pg = require('pg');
 var Client = require('pg').Client;
 
@@ -18,32 +18,32 @@ function wait(ms) {
 }
 
   function schedConv(newBot, response) {
+    var msg = "";
+    var user = [];
     var client = new Client(process.env.DATABASE_URL);
     client.connect();
-    var rows2 = [];
     var query1 = client.query("SELECT message FROM batchmessage WHERE sendtime >= CURRENT_TIMESTAMP - INTERVAL '299.999 seconds' AND sendtime <= CURRENT_TIMESTAMP + INTERVAL '5 minutes' ORDER BY sendtime");
         query1.on('row', function(row1) {
-          var query2 = client.query("SELECT smoochid FROM conversation WHERE smoochid = 'a30fa820d0a0f0216fa26070' LIMIT 30;");
-          //var query2 = client.query("select distinct smoochid from conversation;");
-            query2.on('row2',function(row2) {
-              rows2.push(row2);
-              var msg = process.env.SCHED_PREFIX + row1.message;
-              var p = Promise.resolve();
-              _.each(rows2, function(row2) {
-                console.log("after each");
-                newBot.userId = row2.smoochid;
-                p = p.then(function() {
-                  console.log("before p ", msg);
-                  return wait(50).then(function() {
-                    return bot.say(msg);
-                    console.log('newBot.userId: ', newBot.userId);
-                  });
-                  });
-                  return p.then(() => 'speak');
-                });
-            });
-            });
+          //var query2 = client.query("SELECT smoochid FROM conversation WHERE smoochid = 'a30fa820d0a0f0216fa26070' LIMIT 30;");
+          var msg = row1.message;
+        });
+
+    var query2 = client.query("select distinct smoochid from conversation;");
+      query2.on('row',function(row2) {
+      user.push(row2);
+      });
       client.on('drain', client.end.bind(client));
+
+    _.each(user, function(uid) {
+      p = p.then(function() {
+        return wait(50).then(function(){
+          bot.userId = uid;
+          return bot.say(msg);
+        });
+      });
+    });
+    return p.then(() => 'speak');
   }
+
 
 module.exports = {schedConv};
