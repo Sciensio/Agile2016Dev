37acf52a8b723553b78db99b22bf264210d42045
Sciensio <chris.colleran@gmail.com>
2016-07-25 17:18:31 -0400

    38

--- push.js
+++ push.js
@@ -30,8 +30,8 @@ function wait(ms) {
               rows2.push(row2);
             //});
               var msg = process.env.SCHED_PREFIX + row1.message;
-              console.log(rows2.length);
               _.each(rows2, function(row2) {
+                console.log(row2);
                 newBot.userId = row2;
                 p = p.then(function() {
                   console.log('newBot.userId: ', newBot.userId);
