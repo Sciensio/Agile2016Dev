0e60adb4165a3b5f688bc8d4586ea97c788546e3
Sciensio <chris.colleran@gmail.com>
2016-07-26 07:14:48 -0400

    5

--- push.js
+++ push.js
@@ -26,16 +26,20 @@ pg.defaults.ssl = true;
     client.connect();
 
     var query1 = client.query("SELECT message FROM batchmessage WHERE sendtime >= CURRENT_TIMESTAMP - INTERVAL '299.999 seconds' AND sendtime <= CURRENT_TIMESTAMP + INTERVAL '5 minutes' ORDER BY sendtime");
-        console.log("ran query");
-        query1.on('row', function(row1) {
-          if (row1.message) {
-            var msg = row1.message;
-            getUsers(newBot,client, msg);
-          } else {
-            client.on('drain', client.end.bind(client));
-            return console.log("No messages to be sent");
-          }
-        });
+      console.log("ran query");
+      query1.on('row', function(row1, err) {
+        if (row1.message) {
+          var msg = row1.message;
+          getUsers(newBot,client, msg);
+        } else {
+          client.on('drain', client.end.bind(client));
+          return console.log("No messages to be sent");
+        }
+        if(err) {
+          client.on('drain', client.end.bind(client));
+          return console.error("|| ", err);
+        }
+      });
   }
 
   function getUsers(bot,client, msg) {
