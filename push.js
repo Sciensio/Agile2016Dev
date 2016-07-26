33fc86f83ba557817bf86c80718ff6f53d67d3c0
Sciensio <chris.colleran@gmail.com>
2016-07-25 22:58:36 -0400

    7

--- push.js
+++ push.js
@@ -10,20 +10,22 @@ var pg = require('pg');
 var Client = require('pg').Client;
 const _ = require('lodash');
 
-
 pg.defaults.ssl = true;
 
-function wait(ms) {
-    return new Promise((resolve) => {
-        setTimeout(resolve, ms);
-    });
-}
+  function wait(ms) {
+      return new Promise((resolve) => {
+          setTimeout(resolve, ms);
+      });
+  }
+
+  var msg = "";
 
   function schedConv(newBot, response) {
-    var msg = "";
+
     var user = [];
     var client = new Client(process.env.DATABASE_URL);
     client.connect();
+
     var query1 = client.query("SELECT message FROM batchmessage WHERE sendtime >= CURRENT_TIMESTAMP - INTERVAL '299.999 seconds' AND sendtime <= CURRENT_TIMESTAMP + INTERVAL '5 minutes' ORDER BY sendtime");
         query1.on('row', function(row1) {
           //var query2 = client.query("SELECT smoochid FROM conversation WHERE smoochid = 'a30fa820d0a0f0216fa26070' LIMIT 30;");
@@ -36,11 +38,11 @@ function wait(ms) {
       //  console.log(user);
       });
 
-    query2.on('end', function(result){
-      console.log(user);
-      sayMsg(user);
-    })
-    client.on('drain', client.end.bind(client));
+      query2.on('end', function(result){
+        console.log(user);
+        sayMsg(user);
+      })
+      client.on('drain', client.end.bind(client));
   }
 
   function sayMsg(users) {
