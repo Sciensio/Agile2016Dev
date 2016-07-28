09ba35cc22ac4eb97b1cbd75ab3ce005ca96875b
Sciensio <chris.colleran@gmail.com>
2016-07-28 14:34:02 -0400

    test2

--- script.js
+++ script.js
@@ -33,8 +33,6 @@ module.exports = new Script({
     speak: {
         receive: (bot, message) => {
 
-            bot.say("chris").then(() => 'speak');
-
             if (message.text.indexOf('851557_369239266556155_759568595' > -1)) {
               return bot.say("(y)").then(() => 'speak');
             }
